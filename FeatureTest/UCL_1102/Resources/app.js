972f509e076a84a1e4a8c4314e0c83a8c2dda0e6
Wilson Luu <wluu@appcelerator.com>
2012-03-22 17:43:52 -0700

    Added test case 1098 - 1104

---
+++ FeatureTest/UCL_1102/Resources/app.js
@@ -0,0 +1,14 @@
+var win = Ti.UI.createWindow({
+	backgroundColor:'black',
+	width:Ti.UI.SIZE,
+	height:Ti.UI.SIZE
+});
+
+var view = Ti.UI.createView({
+	backgroundColor:'white',
+	width:2,
+	height:2
+});
+
+win.add(view);
+win.open();
