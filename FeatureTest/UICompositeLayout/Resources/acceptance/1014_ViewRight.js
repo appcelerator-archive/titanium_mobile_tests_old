dfc56777f6f8d22cbf892c5ddec98cd8c85752a3
mpettiford <mpettiford@appcelerator.com>
2012-03-24 14:37:51 -0700

    Updated test case

--- FeatureTest/UICompositeLayout/Resources/acceptance/1014_ViewRight.js
+++ FeatureTest/UICompositeLayout/Resources/acceptance/1014_ViewRight.js
@@ -9,7 +9,7 @@ var view = Ti.UI.createView({
 })
 
 var label = Ti.UI.createLabel({
-	text: 'view.right: ' + view.right + ' The red view should be shifted 10 system units to the left',
+	text: 'view.right value should be 10 and is: ' + view.right + ' \nThe red view should be shifted 10 system units from the right and is :' + view.right,
 	top: 20,
 	left: 10,
 	height: Ti.UI.SIZE,
