f4c88801e4b2a38c86581e47293ce10c6a986799
mpettiford <mpettiford@appcelerator.com>
2012-03-24 13:38:14 -0700

    Updating test case

--- FeatureTest/UICompositeLayout/Resources/acceptance/1033a_UndefinedWidthCenterLeft.js
+++ FeatureTest/UICompositeLayout/Resources/acceptance/1033a_UndefinedWidthCenterLeft.js
@@ -19,7 +19,7 @@ var view = Ti.UI.createView({
 
 win.addEventListener('postlayout', function layoutHandler(e) {
 	win.removeEventListener('postlayout', layoutHandler);
-	label.text = 'View Dynamic Width value: 2*('+ view.center.x +'-'+ view.rect.x + '-' + win.rect.x + ')=' + view.size.width +'\n Static Width Value is undefined:' + view.width;
+	label.text = 'View dynamic width value: 2*('+ view.center.x +'-'+ view.rect.x + '-' + win.rect.x + ') = ' + view.size.width +'\n Static width value should be undefined and is:' + view.width;
 	win.addEventListener('postlayout', layoutHandler);
 });
 
