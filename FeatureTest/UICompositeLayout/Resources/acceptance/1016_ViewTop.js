49c94afc1d9bfe848bd1526aa2db8650f2fc4133
mpettiford <mpettiford@appcelerator.com>
2012-03-24 14:41:56 -0700

    Updating test case

--- FeatureTest/UICompositeLayout/Resources/acceptance/1016_ViewTop.js
+++ FeatureTest/UICompositeLayout/Resources/acceptance/1016_ViewTop.js
@@ -9,7 +9,7 @@ var view = Ti.UI.createView({
 })
 
 var label = Ti.UI.createLabel({
-	text: 'view.top: ' + view.top + ' The red view should be shifted 10 system units down',
+	text: 'view.top value should be 10 and is: ' + view.top + '\nThe red view should be shifted 10 system units from the top and is: ' + view.top,
 	top: 20,
 	left: 10,
 	height: Ti.UI.SIZE,
