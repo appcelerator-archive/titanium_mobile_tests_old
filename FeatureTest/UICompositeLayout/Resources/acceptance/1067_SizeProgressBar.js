69b49856746d9f0044a9112b01bc3ce722bf1113
Wilson Luu <wluu@appcelerator.com>
2012-03-25 11:11:00 -0700

    Made black view larger so progress bars are visible.

--- FeatureTest/UICompositeLayout/Resources/acceptance/1067_SizeProgressBar.js
+++ FeatureTest/UICompositeLayout/Resources/acceptance/1067_SizeProgressBar.js
@@ -14,44 +14,72 @@ var label = Ti.UI.createLabel({
 	color: 'black'
 });
 
+// ios params
+var height1 = 40;
+var height2 = 50;
+var height3 = 70;
+
+var width1 = 60;
+var width2 = 120;
+var width3 = 180;
+
+var top1 = 80;
+var top2 = 140;
+var top3 = 220;
+
+if (Ti.Platform.osname !== 'iphone' && Ti.Platform.osname !== 'ipad'){
+	height1 = 75;
+	height2 = 105;
+	height3 = 115;
+	
+	width1 = 95;
+	width2 = 150;
+	
+	top2 = 175;
+	top3 = 285;
+}
+
 var view1 = Ti.UI.createView({
-	height: 30,
-	width: 60,
-	top: 80,
+	height: height1,
+	width: width1,
+	top: top1,
 	backgroundColor: 'black'
 });
 
 var pb1=Titanium.UI.createProgressBar({
     min: 0,
     max: 10,
+    value:0,
     message: 'Test',
     color: 'white'
 });
 
 var view2 = Ti.UI.createView({
-	height: 50,
-	width: 120,
-	top: 140,
+	height: height2,
+	width: width2,
+	top: top2,
 	backgroundColor: 'black'
 });
 
 var pb2=Titanium.UI.createProgressBar({
     min: 0,
     max: 10,
+    value:0,
     message: 'Short Progress',
     color: 'white'
 });
 
 var view3 = Ti.UI.createView({
-	height: 70,
-	width: 180,
-	top: 220,
+	height: height3,
+	width: width3,
+	top: top3,
 	backgroundColor: 'black'
 });
 
 var pb3=Titanium.UI.createProgressBar({
     min: 0,
     max: 10,
+    value:0,
     message: 'Very long Progress Bar',
     color: 'white'
 });
