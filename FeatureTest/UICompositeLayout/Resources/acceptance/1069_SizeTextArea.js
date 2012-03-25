73fcd3cb47d0b183392a6a0b3c90ebe1bef9f1fd
Wilson Luu <wluu@appcelerator.com>
2012-03-24 17:51:53 -0700

    Modified test to see paragraph

--- FeatureTest/UICompositeLayout/Resources/acceptance/1069_SizeTextArea.js
+++ FeatureTest/UICompositeLayout/Resources/acceptance/1069_SizeTextArea.js
@@ -13,10 +13,35 @@ var label = Ti.UI.createLabel({
 	color: 'black'
 });
 
+// ios widths
+var width1 = 60;
+var width2 = 180;
+
+var height1 = 30;
+var height2 = 50;
+var height3 = 70;
+
+var top1 = 80;
+var top2 = 140;
+var top3 = 220;
+
+if(Ti.Platform.osname !== 'iphone' && Ti.Platform.osname !== 'ipad'){
+	width1 = 180;
+	width2 = 350;
+	
+	height1 = 120;
+	height2 = 210;
+	height3 = 600;
+	
+	top1 = 120;
+	top2 = 250;
+	top3 = 470;
+}
+
 var view1 = Ti.UI.createView({
-	height: 30,
-	width: 60,
-	top: 80,
+	height: height1,
+	width: width1,
+	top: top1,
 	backgroundColor: 'green'
 });
 
@@ -26,21 +51,21 @@ var ta1=Titanium.UI.createTextArea({
 });
 
 var view2 = Ti.UI.createView({
-	height: 50,
-	width: 120,
-	top: 140,
+	height: height2,
+	width: width2,
+	top: top2,
 	backgroundColor: 'green'
 });
 
 var ta2=Titanium.UI.createTextArea({
-	value: 'Sentence: This is to test the text area to display a single sentence',
+	value: 'Sentence: This is to test a single sentence',
 	backgroundColor: 'yellow'
 });
 
 var view3 = Ti.UI.createView({
-	height: 70,
-	width: 180,
-	top: 220,
+	height: height3,
+	width: Ti.UI.SIZE,
+	top: top3,
 	backgroundColor: 'green'
 });
 
